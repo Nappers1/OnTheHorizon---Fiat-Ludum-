1f17e35173b5cdf059597b211a1fd360a7143464
RawGithub17 <60084309+RawGithub17@users.noreply.github.com>
2026-04-11 16:23:46 -0700

    Started working on enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private Transform[] spawnPoints; //0 is top, 1,2 ,3, 4 top right down left (clockwise
+    [SerializeField] private GameObject enemy;
+    //[SerializeField] private Transform centerPos; 
+
+    private int waveCount = 0;
+    private int numDirections = 4; 
+    [SerializeField] private int enemyCount = 4;
+    private void Start()
+    {
+        waveCount = 0;
+        WaveStart();
+    }
+
+    private void WaveStart()
+    {
+        for (int i = 0; i < enemyCount; i++)
+        {
+            int posIndex = UnityEngine.Random.Range(0, numDirections - 1);
+            Instantiate(enemy, spawnPoints[posIndex]);
+            Debug.Log(posIndex);
+        }
+        waveCount++;
+    }
+}
